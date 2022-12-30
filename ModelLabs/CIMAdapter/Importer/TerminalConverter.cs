3a3f834787c2513fd3b7bd564a7106e7fdd5fe68
Dusan-Ivic <ivicd99@gmail.com>
2022-12-30 20:58:06 +0100

    Implement Importer and Converter functions

---
+++ ModelLabs/CIMAdapter/Importer/TerminalConverter.cs
@@ -0,0 +1,203 @@
+﻿namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
+{
+	using FTN.Common;
+
+	/// <summary>
+	/// TerminalConverter has methods for populating
+	/// ResourceDescription objects using TerminalCIMProfile_Labs objects.
+	/// </summary>
+	public static class TerminalConverter
+    {
+
+		#region Populate ResourceDescription
+        
+		public static void PopulateIdentifiedObjectProperties(FTN.IdentifiedObject cimIdentifiedObject, ResourceDescription rd)
+		{
+			if ((cimIdentifiedObject != null) && (rd != null))
+			{
+				if (cimIdentifiedObject.AliasNameHasValue)
+				{
+					rd.AddProperty(new Property(ModelCode.IDOBJ_ALIAS, cimIdentifiedObject.AliasName));
+				}
+                if (cimIdentifiedObject.MRIDHasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.IDOBJ_MRID, cimIdentifiedObject.MRID));
+                }
+                if (cimIdentifiedObject.NameHasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.IDOBJ_NAME, cimIdentifiedObject.Name));
+                }
+            }
+		}
+        
+        public static void PopulateTerminalProperties(FTN.Terminal cimTerminal, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimTerminal != null) && (rd != null))
+            {
+                TerminalConverter.PopulateIdentifiedObjectProperties(cimTerminal, rd);
+
+                if (cimTerminal.ConnectedHasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.TERMINAL_CONNECTED, cimTerminal.Connected));
+                }
+                if (cimTerminal.PhasesHasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.TERMINAL_PHASES, (short)GetDMSPhaseCode(cimTerminal.Phases)));
+                }
+                if (cimTerminal.SequenceNumberHasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.TERMINAL_SEQNUM, cimTerminal.SequenceNumber));
+                }
+                if (cimTerminal.ConductingEquipmentHasValue)
+                {
+                    long gid = importHelper.GetMappedGID(cimTerminal.ConductingEquipment.ID);
+                    if (gid < 0)
+                    {
+                        report.Report.Append("WARNING: Convert ").Append(cimTerminal.GetType().ToString()).Append(" rdfID = \"").Append(cimTerminal.ID);
+                        report.Report.Append("\" - Failed to set reference to ConductingEquipment: rdfID \"").Append(cimTerminal.ConductingEquipment.ID).AppendLine(" \" is not mapped to GID!");
+                    }
+                    rd.AddProperty(new Property(ModelCode.TERMINAL_CONDEQ, gid));
+                }
+                if (cimTerminal.ConnectivityNodeHasValue)
+                {
+                    long gid = importHelper.GetMappedGID(cimTerminal.ConnectivityNode.ID);
+                    if (gid < 0)
+                    {
+                        report.Report.Append("WARNING: Convert ").Append(cimTerminal.GetType().ToString()).Append(" rdfID = \"").Append(cimTerminal.ID);
+                        report.Report.Append("\" - Failed to set reference to ConnectivityNode: rdfID \"").Append(cimTerminal.ConnectivityNode.ID).AppendLine(" \" is not mapped to GID!");
+                    }
+                    rd.AddProperty(new Property(ModelCode.TERMINAL_NODE, gid));
+                }
+            }
+        }
+        
+        public static void PopulateConnectivityNodeProperties(FTN.ConnectivityNode cimConnectivityNode, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimConnectivityNode != null) && (rd != null))
+            {
+                TerminalConverter.PopulateIdentifiedObjectProperties(cimConnectivityNode, rd);
+            }
+        }
+
+        public static void PopulatePowerSystemResourceProperties(FTN.PowerSystemResource cimPowerSystemResource, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+		{
+			if ((cimPowerSystemResource != null) && (rd != null))
+			{
+				TerminalConverter.PopulateIdentifiedObjectProperties(cimPowerSystemResource, rd);
+			}
+		}
+
+		public static void PopulateEquipmentProperties(FTN.Equipment cimEquipment, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+		{
+			if ((cimEquipment != null) && (rd != null))
+			{
+                TerminalConverter.PopulatePowerSystemResourceProperties(cimEquipment, rd, importHelper, report);
+			}
+		}
+
+		public static void PopulateConductingEquipmentProperties(FTN.ConductingEquipment cimConductingEquipment, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+		{
+			if ((cimConductingEquipment != null) && (rd != null))
+			{
+                TerminalConverter.PopulateEquipmentProperties(cimConductingEquipment, rd, importHelper, report);
+			}
+		}
+        
+        public static void PopulateRectifierInverterProperties(FTN.RectifierInverter cimRectifierInverter, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimRectifierInverter != null) && (rd != null))
+            {
+                TerminalConverter.PopulateConductingEquipmentProperties(cimRectifierInverter, rd, importHelper, report);
+            }
+        }
+        
+        public static void PopulateConductorProperties(FTN.Conductor cimConductor, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimConductor != null) && (rd != null))
+            {
+                TerminalConverter.PopulateConductingEquipmentProperties(cimConductor, rd, importHelper, report);
+            }
+        }
+        
+        public static void PopulateACLineSegmentProperties(FTN.ACLineSegment cimACLineSegment, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimACLineSegment != null) && (rd != null))
+            {
+                TerminalConverter.PopulateConductorProperties(cimACLineSegment, rd, importHelper, report);
+            }
+        }
+        
+        public static void PopulateClampProperties(FTN.Clamp cimClamp, ResourceDescription rd, ImportHelper importHelper, TransformAndLoadReport report)
+        {
+            if ((cimClamp != null) && (rd != null))
+            {
+                TerminalConverter.PopulateConductingEquipmentProperties(cimClamp, rd, importHelper, report);
+
+                if (cimClamp.LengthFromTerminal1HasValue)
+                {
+                    rd.AddProperty(new Property(ModelCode.CLAMP_LENGTHFROMTERMINAL, cimClamp.LengthFromTerminal1));
+                }
+                if (cimClamp.ACLineSegmentHasValue)
+                {
+                    long gid = importHelper.GetMappedGID(cimClamp.ACLineSegment.ID);
+                    if (gid < 0)
+                    {
+                        report.Report.Append("WARNING: Convert ").Append(cimClamp.GetType().ToString()).Append(" rdfID = \"").Append(cimClamp.ID);
+                        report.Report.Append("\" - Failed to set reference to ACLineSegment: rdfID \"").Append(cimClamp.ACLineSegment.ID).AppendLine(" \" is not mapped to GID!");
+                    }
+                    rd.AddProperty(new Property(ModelCode.CLAMP_ACLINESEGMENT, gid));
+                }
+            }
+        }
+
+        #endregion Populate ResourceDescription
+
+        #region Enums convert
+
+        public static PhaseCode GetDMSPhaseCode(FTN.PhaseCode phases)
+		{
+			switch (phases)
+			{
+				case FTN.PhaseCode.A:
+					return PhaseCode.A;
+				case FTN.PhaseCode.AB:
+					return PhaseCode.AB;
+				case FTN.PhaseCode.ABC:
+					return PhaseCode.ABC;
+				case FTN.PhaseCode.ABCN:
+					return PhaseCode.ABCN;
+				case FTN.PhaseCode.ABN:
+					return PhaseCode.ABN;
+				case FTN.PhaseCode.AC:
+					return PhaseCode.AC;
+				case FTN.PhaseCode.ACN:
+					return PhaseCode.ACN;
+				case FTN.PhaseCode.AN:
+					return PhaseCode.AN;
+				case FTN.PhaseCode.B:
+					return PhaseCode.B;
+				case FTN.PhaseCode.BC:
+					return PhaseCode.BC;
+				case FTN.PhaseCode.BCN:
+					return PhaseCode.BCN;
+				case FTN.PhaseCode.BN:
+					return PhaseCode.BN;
+				case FTN.PhaseCode.C:
+					return PhaseCode.C;
+				case FTN.PhaseCode.CN:
+					return PhaseCode.CN;
+				case FTN.PhaseCode.N:
+					return PhaseCode.N;
+				case FTN.PhaseCode.s12N:
+					return PhaseCode.ABN;
+				case FTN.PhaseCode.s1N:
+					return PhaseCode.AN;
+				case FTN.PhaseCode.s2N:
+					return PhaseCode.BN;
+				default: return PhaseCode.Unknown;
+			}
+		}
+        
+		#endregion Enums convert
+	}
+}
---
+++ ModelLabs/CIMAdapter/Importer/TerminalImporter.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using CIM.Model;
+using FTN.Common;
+using FTN.ESI.SIMES.CIM.CIMAdapter.Manager;
+
+namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
+{
+    /// <summary>
+    /// TerminalImporter
+    /// </summary>
+    public class TerminalImporter
+	{
+		/// <summary> Singleton </summary>
+		private static TerminalImporter tImporter = null;
+		private static object singletoneLock = new object();
+
+		private ConcreteModel concreteModel;
+		private Delta delta;
+		private ImportHelper importHelper;
+		private TransformAndLoadReport report;
+
+
+		#region Properties
+		public static TerminalImporter Instance
+		{
+			get
+			{
+				if (tImporter == null)
+				{
+					lock (singletoneLock)
+					{
+						if (tImporter == null)
+						{
+							tImporter = new TerminalImporter();
+							tImporter.Reset();
+						}
+					}
+				}
+				return tImporter;
+			}
+		}
+
+		public Delta NMSDelta
+		{
+			get 
+			{
+				return delta;
+			}
+		}
+		#endregion Properties
+
+
+		public void Reset()
+		{
+			concreteModel = null;
+			delta = new Delta();
+			importHelper = new ImportHelper();
+			report = null;
+		}
+
+		public TransformAndLoadReport CreateNMSDelta(ConcreteModel cimConcreteModel)
+		{
+			LogManager.Log("Importing Terminal Elements...", LogLevel.Info);
+			report = new TransformAndLoadReport();
+			concreteModel = cimConcreteModel;
+			delta.ClearDeltaOperations();
+
+			if ((concreteModel != null) && (concreteModel.ModelMap != null))
+			{
+				try
+				{
+					// convert into DMS elements
+					ConvertModelAndPopulateDelta();
+				}
+				catch (Exception ex)
+				{
+					string message = string.Format("{0} - ERROR in data import - {1}", DateTime.Now, ex.Message);
+					LogManager.Log(message);
+					report.Report.AppendLine(ex.Message);
+					report.Success = false;
+				}
+			}
+			LogManager.Log("Importing Terminal Elements - END.", LogLevel.Info);
+			return report;
+		}
+
+		/// <summary>
+		/// Method performs conversion of network elements from CIM based concrete model into DMS model.
+		/// </summary>
+		private void ConvertModelAndPopulateDelta()
+		{
+			LogManager.Log("Loading elements and creating delta...", LogLevel.Info);
+            
+            ImportRectifierInverters();
+            ImportACLineSegments();
+            ImportClamps();
+            ImportConnectivityNodes();
+            ImportTerminals();
+
+			LogManager.Log("Loading elements and creating delta completed.", LogLevel.Info);
+		}
+
+        #region Import
+        
+        private void ImportRectifierInverters()
+        {
+            SortedDictionary<string, object> cimRectifierInverters = concreteModel.GetAllObjectsOfType("FTN.RectifierInverter");
+            if (cimRectifierInverters != null)
+            {
+                foreach (KeyValuePair<string, object> cimRectifierInverterPair in cimRectifierInverters)
+                {
+                    FTN.RectifierInverter cimRectifierInverter = cimRectifierInverterPair.Value as FTN.RectifierInverter;
+
+                    ResourceDescription rd = CreateRectifierInverterResourceDescription(cimRectifierInverter);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("RectifierInverter ID = ").Append(cimRectifierInverter.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("RectifierInverter ID = ").Append(cimRectifierInverter.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+        
+        private void ImportACLineSegments()
+        {
+            SortedDictionary<string, object> cimACLineSegments = concreteModel.GetAllObjectsOfType("FTN.ACLineSegment");
+            if (cimACLineSegments != null)
+            {
+                foreach (KeyValuePair<string, object> cimACLineSegmentPair in cimACLineSegments)
+                {
+                    FTN.ACLineSegment cimACLineSegment = cimACLineSegmentPair.Value as FTN.ACLineSegment;
+
+                    ResourceDescription rd = CreateACLineSegmentResourceDescription(cimACLineSegment);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("ACLineSegment ID = ").Append(cimACLineSegment.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("ACLineSegment ID = ").Append(cimACLineSegment.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+        
+        private void ImportClamps()
+        {
+            SortedDictionary<string, object> cimClamps = concreteModel.GetAllObjectsOfType("FTN.Clamp");
+            if (cimClamps != null)
+            {
+                foreach (KeyValuePair<string, object> cimClampPair in cimClamps)
+                {
+                    FTN.Clamp cimClamp = cimClampPair.Value as FTN.Clamp;
+
+                    ResourceDescription rd = CreateClampResourceDescription(cimClamp);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("Clamp ID = ").Append(cimClamp.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("Clamp ID = ").Append(cimClamp.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+        
+        private void ImportConnectivityNodes()
+        {
+            SortedDictionary<string, object> cimConnectivityNodes = concreteModel.GetAllObjectsOfType("FTN.ConnectivityNode");
+            if (cimConnectivityNodes != null)
+            {
+                foreach (KeyValuePair<string, object> cimConnectivityNodePair in cimConnectivityNodes)
+                {
+                    FTN.ConnectivityNode cimConnectivityNode = cimConnectivityNodePair.Value as FTN.ConnectivityNode;
+
+                    ResourceDescription rd = CreateConnectivityNodeResourceDescription(cimConnectivityNode);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("ConnectivityNode ID = ").Append(cimConnectivityNode.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("ConnectivityNode ID = ").Append(cimConnectivityNode.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+        
+        private void ImportTerminals()
+        {
+            SortedDictionary<string, object> cimTerminals = concreteModel.GetAllObjectsOfType("FTN.Terminal");
+            if (cimTerminals != null)
+            {
+                foreach (KeyValuePair<string, object> cimTerminalPair in cimTerminals)
+                {
+                    FTN.Terminal cimTerminal = cimTerminalPair.Value as FTN.Terminal;
+
+                    ResourceDescription rd = CreateTerminalResourceDescription(cimTerminal);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("Terminal ID = ").Append(cimTerminal.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("Terminal ID = ").Append(cimTerminal.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        #endregion Import
+
+        #region Create
+
+        private ResourceDescription CreateRectifierInverterResourceDescription(FTN.RectifierInverter cimRectifierInverter)
+        {
+            ResourceDescription rd = null;
+            if (cimRectifierInverter != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.RECTIFIERINVERTER, importHelper.CheckOutIndexForDMSType(DMSType.RECTIFIERINVERTER));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimRectifierInverter.ID, gid);
+
+                ////populate ResourceDescription
+                TerminalConverter.PopulateRectifierInverterProperties(cimRectifierInverter, rd, importHelper, report);
+            }
+            return rd;
+        }
+        
+        private ResourceDescription CreateACLineSegmentResourceDescription(FTN.ACLineSegment cimACLineSegment)
+        {
+            ResourceDescription rd = null;
+            if (cimACLineSegment != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.ACLINESEGMENT, importHelper.CheckOutIndexForDMSType(DMSType.ACLINESEGMENT));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimACLineSegment.ID, gid);
+
+                ////populate ResourceDescription
+                TerminalConverter.PopulateACLineSegmentProperties(cimACLineSegment, rd, importHelper, report);
+            }
+            return rd;
+        }
+        
+        private ResourceDescription CreateClampResourceDescription(FTN.Clamp cimClamp)
+        {
+            ResourceDescription rd = null;
+            if (cimClamp != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.CLAMP, importHelper.CheckOutIndexForDMSType(DMSType.CLAMP));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimClamp.ID, gid);
+
+                ////populate ResourceDescription
+                TerminalConverter.PopulateClampProperties(cimClamp, rd, importHelper, report);
+            }
+            return rd;
+        }
+        
+        private ResourceDescription CreateConnectivityNodeResourceDescription(FTN.ConnectivityNode cimConnectivityNode)
+        {
+            ResourceDescription rd = null;
+            if (cimConnectivityNode != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.CONNECTIVITYNODE, importHelper.CheckOutIndexForDMSType(DMSType.CONNECTIVITYNODE));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimConnectivityNode.ID, gid);
+
+                ////populate ResourceDescription
+                TerminalConverter.PopulateConnectivityNodeProperties(cimConnectivityNode, rd, importHelper, report);
+            }
+            return rd;
+        }
+        
+        private ResourceDescription CreateTerminalResourceDescription(FTN.Terminal cimTerminal)
+        {
+            ResourceDescription rd = null;
+            if (cimTerminal != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.TERMINAL, importHelper.CheckOutIndexForDMSType(DMSType.TERMINAL));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimTerminal.ID, gid);
+
+                ////populate ResourceDescription
+                TerminalConverter.PopulateTerminalProperties(cimTerminal, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        #endregion Create
+    }
+}
+
