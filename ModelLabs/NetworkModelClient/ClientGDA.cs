e35374a35af300674d43c9464b5a01740c5ee414
Dusan-Ivic <ivicd99@gmail.com>
2023-01-03 20:50:51 +0100

    Create Network Model Client as WPF application

---
+++ ModelLabs/NetworkModelClient/ClientGDA.cs
@@ -0,0 +1,49 @@
+﻿using FTN.Common;
+using FTN.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkModelClient
+{
+    public class ClientGDA : IDisposable
+    {
+        private ModelResourcesDesc modelResourcesDesc = new ModelResourcesDesc();
+        private NetworkModelGDAProxy gdaQueryProxy = null;
+
+        public ClientGDA()
+        {
+
+        }
+
+        private NetworkModelGDAProxy GdaQueryProxy
+        {
+            get
+            {
+                if (gdaQueryProxy != null)
+                {
+                    gdaQueryProxy.Abort();
+                    gdaQueryProxy = null;
+                }
+
+                gdaQueryProxy = new NetworkModelGDAProxy("NetworkModelGDAEndpoint");
+                gdaQueryProxy.Open();
+
+                return gdaQueryProxy;
+            }
+        }
+
+        #region GDAQueryService
+
+        // TODO - GDA methods
+
+        #endregion GDAQueryService
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
