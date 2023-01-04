e500183ff55df65746e5cbeaf41783742373e25e
Dusan-Ivic <ivicd99@gmail.com>
2023-01-04 16:29:12 +0100

    Fix property display when value is a collection

---
+++ ModelLabs/NetworkModelClient/Converters/PropertyTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using FTN.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NetworkModelClient.Converters
+{
+    public class PropertyTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Property property = (Property)value;
+
+            return (property.GetValue() is System.Collections.IList) ? "Vector" : "Single";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
